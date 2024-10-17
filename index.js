a3eeed5fde904cbc90c60f0eabda2bd215977d0c
Mihai-sys <mihaicostros7@gmail.com>
2024-10-17 17:09:34 +0200

    game

--- index.js
+++ index.js
@@ -1,53 +1,69 @@
 class Game {
     constructor() {
-        this.numberToGuess = Math.floor(Math.random() * 11);
-
+        this.numberToGuess = NaN;
+        this.generateNumber();
+        this.isStarted = false;
     }
 
     startGame() {
         console.log("Game started");
-        this.setup();
+        if (!this.isStarted) {
+            console.log('starting new')
+            this.setup();
+            this.isStarted = true;
+        } else {
+            this.generateNumber();
+            const result = document.getElementById("result");
+            result.innerHTML = '';
+        }
+        
+    }
+
+    generateNumber() {
+        this.numberToGuess = Math.floor(Math.random() * 11);
     }
 
     setup() {
-        const input = `<label for="guess">
-        Introduceti un numar intre 0 si 10!
+        const input = `
+        <label for="guess">
+            Introduceti un numar intre 0 si 10! 
         </label>
-        <input id="guess" "type=number">
-        <button id= "check-answer">Verificaraspuns</button>`;
+        <input id="guess" type="number">
+        <button id="check-answer">Verifica raspuns</button>`;
         const container = document.createElement("div");
         container.innerHTML = input;
         const gameContainer = document.querySelector(".game-container");
         gameContainer.appendChild(container);
-        this.setupCheckButton;
+        this.setupCheckButton();
     }
 
-}
-
-setupCheckButton() {
-    const btn = document.getElementById("check-answer");
-    const input = document.getElementById("guess");
-    const valueAsInt = parseInt(input.value);
-    btn.addEventListener("click", () => this.checkAnswer(input.value));
-}
-
-checkAnswer(answer) {
-   const result = document.getElementById("result");
-    if (answer === this.numberToGuess){
-        
-        result.textContent = "Ai ghicit";
-        result.className = "success";
-    }else{
-        console.log("Mai incearca!");
-        result.textContent = "Mai incearca!";
+    setupCheckButton() {
+        const btn = document.getElementById("check-answer");
+       
+        btn.addEventListener("click", () => { 
+            const input = document.getElementById("guess");
+            const valueAsInt = parseInt(input.value);
+            this.checkAnswer(valueAsInt)
+        }); 
     }
 
+    checkAnswer(answer) {
+        const result = document.getElementById("result");
+        if (answer === this.numberToGuess) {
+            result.textContent = "Ai ghicit!";
+            result.className = "success";
+        } else {
+            result.textContent = "Mai incearca!";
+            result.className = "error";
+        }
+    }
 }
 
 const startGameBtn = document.querySelector(".game-container > button");
-startGameBtn.addEventListener("click", () =>{
-    const game = new Game();
+const game = new Game();
+
+startGameBtn.addEventListener("click", () => {
     game.startGame();
     console.log(game);
-    
+    startGameBtn.textContent = "Reseteaza joc!"
 })
